0a5c6c3b3bb265bc568f846af9b9e5ac0ad659c2
Charlie-Foy-117 <83784786+Charlie-Foy-117@users.noreply.github.com>
2022-09-25 21:30:55 +0100

    Added features

--- AdventureGame/AdventureGame.cpp
+++ AdventureGame/AdventureGame.cpp
@@ -125,19 +125,19 @@ int main()
     Area wailingMarsh;
     wailingMarsh.name = "Wailing Marsh";
     wailingMarsh.description = "The corrupted lands that speaks";
-    wailingMarsh.contents = {"Trees", "Wildlife", "Ponds"};
+    wailingMarsh.contents = {"Swamp Dweller", "Health Potion", "Swords"};
     wailingMarsh.exits = { "Northern passge way", "Drowned gateway" };
 
     Area fallenFortress;
     fallenFortress.name = "Fallen Fortress";
     fallenFortress.description = "The last memory of what once was";
-    fallenFortress.contents = {"Rubble", "Swords and Shields", "Wooden Spikes"};
+    fallenFortress.contents = {"Strength Potion", "Rust-EE", "Key"};
     fallenFortress.exits = { "Eastern pathway", "Road to the undercove", "The pit" };
 
     Area cloudedCity;
     cloudedCity.name = "Clouded City";
     cloudedCity.description = "The city above all";
-    cloudedCity.contents = {"Clouds", "Castles", "Golden Furniture"};
+    cloudedCity.contents = {"Lever", "Allfather", "Lock"};
     cloudedCity.exits = { "Stairway up", "The cloud bridge" };
 
     //PrintArea(wailingMarsh);
@@ -145,17 +145,17 @@ int main()
     //PrintArea(cloudedCity);
 
     //feature
-    Feature feature;
-    feature.name = "feature";
-    feature.description = "feature";
+    Feature lock;
+    lock.name = "Lock";
+    lock.description = "Can be unlocked with a key";
 
-    Feature feature2;
-    feature.name = "feature";
-    feature.description = "feature";
+    Feature sword;
+    sword.name = "Sword";
+    sword.description = "Strength potion can be used on it to increase damage";
 
-    Feature feature3;
-    feature.name = "feature";
-    feature.description = "faeture";
+    Feature lever;
+    lever.name = "Lever";
+    lever.description = "When pulled boss fight starts";
 
     //PrintFeature(feature);
     //PrintFeature(feature2);
@@ -166,13 +166,13 @@ int main()
     healthPot.name = "Potion of restoration";
     healthPot.description = "Will heal player for 20 health points";
 
-    Item strePot;
-    strePot.name = "Potion of strength";
-    strePot.description = "Will make next attack done by player deal an addtional 20 points";
+    Item strenPot;
+    strenPot.name = "Potion of strength";
+    strenPot.description = "Will make next attack done by player deal an addtional 20 points";
 
-    Item mossCrown;
-    mossCrown.name = "Crown of Moss";
-    mossCrown.description = "Does nothing, player looks cool";
+    Item key;
+    key.name = "Key";
+    key.description = "Unlocks lock";
 
     //PrintItem(healthPot);
     //PrintItem(strePot);
